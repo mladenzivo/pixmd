6dbfb01949982495e10869392664e49e444dd151
EugeneButovka <eugene.butovka@gmail.com>
2020-06-13 23:45:33 +0300

    fix redirection if in prod

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ import "prismjs/themes/prism-tomorrow.css"
 
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
+
+const { host } = window.location;
+
+// Fix issues with multiple redirect urls.
+// Try to figure out which one to use...
+/*if (awsconfig.oauth.redirectSignIn.includes(',')) {
+  const filterHost = url => new URL(url).host === host;
+  awsconfig.oauth.redirectSignIn = awsconfig.oauth.redirectSignIn
+    .split(',')
+    .filter(filterHost)
+    .shift();
+  awsconfig.oauth.redirectSignOut = awsconfig.oauth.redirectSignOut
+    .split(',')
+    .filter(filterHost)
+    .shift();
+}*/
 Amplify.configure(awsconfig);
 
 const App = props => {
